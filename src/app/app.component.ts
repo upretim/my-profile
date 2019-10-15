1d11eef227ed117f006083edf9d3c1008c847162
Manoj Upreti <manoj-u@HCL.COM>
2019-10-15 16:34:52 +0530

    Added initial first draft code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {ProfilePicComponent} from './profile-pic/profile-pic.component';
+import {AboutComponent} from './about/about.component';
+import {ContactMeComponent} from './contact-me/contact-me.component';
+import {ServicesComponent} from './services/services.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfilePicComponent } from './profile-pic/profile-pic.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilePicComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule
